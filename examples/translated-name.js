cb6e672b6b875350426d4c4e486a74319f35c221
Chris Park <cpark@basistech.com>
2015-09-11 11:36:04 -0400

    publish 0.5.3

--- examples/translated-name.js
+++ examples/translated-name.js
@@ -16,7 +16,7 @@ parser.addArgument(["--key"], {help: "Rosette API key", required: true});
 var args = parser.parseArgs();
 
 var translationParams = new NameTranslationParameters();
-translationParams.setItem("name", "معمر محمد أبو منيار القذافي‎");
+translationParams.setItem("name", "معمر محمد أبو منيار القذاف‎");
 translationParams.setItem("entityType", "PERSON");
 translationParams.setItem("targetLanguage", "eng");
 
