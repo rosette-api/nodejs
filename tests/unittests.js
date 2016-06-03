4e22a8e52858fa18ddf19f823e346f616abd06cb
fiona <fiona@basistech.com>
2016-06-03 11:40:16 -0400

    Unified entities and entities/linked

--- tests/unittests.js
+++ tests/unittests.js
@@ -421,68 +421,6 @@ describe("Entities Endpoint", function() {
 
 });
 
-describe("Entities Linked Endpoint", function() {
-    beforeEach(function(done) {
-        var mockResponse = JSON.stringify({'name':'Rosette API', 'versionChecked':true});
-
-        nock('https://api.rosette.com', {"encodedQueryParams": true })
-           .post('/rest/v1/info')
-           .query({"clientVersion":"1.1"})
-           .reply(200, JSON.parse(mockResponse));
-
-        nock('https://api.rosette.com', {"encodedQueryParams": true })
-           .post('/rest/v1/entities/linked')
-           .query({"clientVersion":"1.1"})
-           .reply(200, JSON.parse(mockResponse));
-        done();
-    });
-
-    afterEach(function(done) {
-        nock.cleanAll();
-        done();
-    });
-    
-    it("successfully calls the entities linked endpoint", function(done) {
-        var api = new Api('123456789', 'https://api.rosette.com/rest/v1');
-        api.parameters.content = "Some Content";
-        api.parameters.linked = true;
-
-        api.rosette("entities", function(err, res) {
-            chai.expect(err).to.be.null;
-            chai.expect(res.name).to.equal('Rosette API');
-            done();
-        });
-
-    });
-
-    it("detects content and contentUri are defined", function(done) {
-        var api = new Api('123456789', 'https://api.rosette.com/rest/v1');
-        api.parameters.content = "Sample Content";
-        api.parameters.contentUri = "http://some.url.com";
-        api.parameters.linked = true;
-
-        api.rosette("entities", function(err, res) {
-            chai.expect(err).to.not.be.null;
-            chai.expect(err.name).to.equal('RosetteException');
-            chai.expect(err.message).to.contain('badArgument');
-            done();
-        });
-    });
-
-    it("detects neither content nor contentUri are defined", function(done) {
-        var api = new Api('123456789', 'https://api.rosette.com/rest/v1');
-        api.parameters.linked = true;
-
-        api.rosette("entities", function(err, res) {
-            chai.expect(err).to.not.be.null;
-            chai.expect(err.name).to.equal('RosetteException');
-            chai.expect(err.message).to.contain('badArgument');
-            done();
-        });
-    });
-
-});
-
 describe("Morphology Endpoint (suite covers all features)", function() {
     beforeEach(function(done) {
         var mockResponse = JSON.stringify({'name':'Rosette API', 'versionChecked':true});
