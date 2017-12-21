73b4c221bcbf23cc56e57f78082683299c2f05f1
Chris Park <cpark@basistech.com>
2017-12-21 15:57:36 -0500

    Example updates

--- examples/language_multilingual.js
+++ examples/language_multilingual.js
@@ -17,7 +17,7 @@ appHeader[0] = "X-RosetteAPI-App"
 appHeader[1] = "app";
 api.parameters.customHeaders = [appHeader];
 
-var language_multilingual_data = "TBD";
+var language_multilingual_data = "On Thursday, as protesters gathered in Washington D.C., the United States Federal Communications Commission under Chairman Ajit Pai voted 3-2 to overturn a 2015 decision, commonly called Net Neutrality, that forbade Internet service providers (ISPs) such as Verizon, Comcast, and AT&T from blocking individual websites or charging websites or customers more for faster load times.  Quatre femmes ont été nommées au Conseil de rédaction de la loi du Qatar. Jeudi, le décret royal du Qatar a annoncé que 28 nouveaux membres ont été nommés pour le Conseil de la Choura du pays.  ذكرت مصادر أمنية يونانية، أن 9 موقوفين من منظمة \"د هـ ك ب ج\" الذين كانت قد أوقفتهم الشرطة اليونانية في وقت سابق كانوا يخططون لاغتيال الرئيس التركي رجب طيب أردوغان.";
 
 api.parameters.content = language_multilingual_data;
 var appHeader = [];
