f49be26da951b4384d75200860c9a6cb583c3832
Chris Park <cpark@basistech.com>
2017-12-21 10:01:03 -0500

    WS-1272 Multilingual example

---
+++ examples/language_multilingual.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var Api = require("../lib/Api");
+var ArgumentParser = require("argparse").ArgumentParser;
+
+var parser = new ArgumentParser({
+  addHelp: true,
+  description: "Determine the language of a piece of text"
+});
+parser.addArgument(["--key"], {help: "Rosette API key", required: true});
+parser.addArgument(["--url"], {help: "Rosette API alt-url", required: false});
+var args = parser.parseArgs();
+var api = new Api(args.key, args.url);
+var endpoint = "language";
+var appHeader = [];
+appHeader[0] = "X-RosetteAPI-App"
+appHeader[1] = "app";
+api.parameters.customHeaders = [appHeader];
+
+var language_multilingual_data = "TBD";
+
+api.parameters.content = language_multilingual_data;
+var appHeader = [];
+appHeader[0] = "X-RosetteAPI-App"
+appHeader[1] = "app";
+api.parameters.customHeaders = [appHeader];
+api.parameters.options = { "multilingual": "true" };
+
+api.rosette(endpoint, function(err, res){
+	if(err){
+		console.log(err);
+	} else {
+		console.log(JSON.stringify(res, null, 2));
+	}
+});
