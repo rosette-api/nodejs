c4f3f2282dfa1efe04e3ce415126fef40dad33eb
Adam Soos <asoos@basistech.com>
2024-11-11 11:18:53 +0100

    WS-3330: fix entities example

--- examples/entities.js
+++ examples/entities.js
@@ -15,16 +15,12 @@ var api = new Api(args.key, args.url);
 var endpoint = "entities";
 var entities_text_data = "The Securities and Exchange Commission today announced the leadership of the agency’s trial unit.  Bridget Fitzpatrick has been named Chief Litigation Counsel of the SEC and David Gottesman will continue to serve as the agency’s Deputy Chief Litigation Counsel. Since December 2016, Ms. Fitzpatrick and Mr. Gottesman have served as Co-Acting Chief Litigation Counsel.  In that role, they were jointly responsible for supervising the trial unit at the agency’s Washington D.C. headquarters as well as coordinating with litigators in the SEC’s 11 regional offices around the country.";
 
-//api.parameters.content = entities_text_data;
+api.parameters.content = entities_text_data;
 
-var result = api.rosette(endpoint, function(err, res){
-    console.log("In examples callback");
-	if(err){
-	    console.log("Returning error");
-		return err;
-	} else {
-	    return res;
-	}
+api.rosette(endpoint, function(err, res){
+  if(err){
+    console.log(err);
+  } else {
+    console.log(JSON.stringify(res, null, 2));
+  }
 });
-
-console.log(result);
