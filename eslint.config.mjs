1a7be6f31d01fe62af729a5cdb25030aa7200529
Seth Gransky <sgransky@babelstreet.com>
2025-01-08 14:17:35 -0600

    NO-JIRA:  Disable no-unused-vars to pass eslint until ESM migration.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,13 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: {
+      globals: globals.node
+    },
+    rules: {
+      'no-unused-vars': 'off' // TODO:  Remove during ESM migration
+    }
+  },
   pluginJs.configs.recommended,
 ];
